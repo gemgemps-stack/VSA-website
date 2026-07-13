e786ca4e86c8fc5d4fb44a0526bf9e8d93837766
gemgemps-stack <gemgemps.stack@example.com>
2026-07-13 23:48:32 +0800

    att page

--- frontend/src/features/attendance/AttendanceView.js
+++ frontend/src/features/attendance/AttendanceView.js
@@ -637,7 +637,7 @@ const calculateDayType = (hours) => {
                 type="text"
                 value={employeeNameQuery}
                 onChange={(e) => setEmployeeNameQuery(e.target.value)}
-                placeholder="Search employee name"
+                placeholder="⌕ Search employee name"
               />
             </div>
 
@@ -647,7 +647,7 @@ const calculateDayType = (hours) => {
                 type="text"
                 value={statusQuery}
                 onChange={(e) => setStatusQuery(e.target.value)}
-                placeholder="Search status"
+                placeholder="⌕ Search status"
               />
             </div>
 
