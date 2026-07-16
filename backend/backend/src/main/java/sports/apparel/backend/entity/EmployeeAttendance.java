d209bc1cf21a2145680758d0bbafc6af0b82f126
gemgemps-stack <gemgemps.stack@example.com>
2026-07-16 21:35:29 +0800

    attempt 2

--- backend/backend/src/main/java/sports/apparel/backend/entity/EmployeeAttendance.java
+++ backend/backend/src/main/java/sports/apparel/backend/entity/EmployeeAttendance.java
@@ -10,6 +10,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
